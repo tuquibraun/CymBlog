0b3226bf0dfed5025a9883012fad6acab02e1004
SantiagoCollomb <santiagocollomb@gmail.com>
2017-09-19 16:27:39 -0300

    mail

---
+++ templates/views/mail.php
@@ -0,0 +1,11 @@
+<?php
+$para = "codeyourmindarg@gmail.com";
+$tema = $_POST['tema'];
+$de = $_POST['nombre']
+$mensaje = $_POST['mensaje'];
+$mail = $_POST['email']
+$headers = "De:". $mail ."\r\n" .
+"Nombre:". $de
+
+mail($para,$tema,$mensaje,$headers);
+?>
